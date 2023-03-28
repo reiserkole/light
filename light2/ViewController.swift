1265b43a89d2380850cbc38ad12f58a84987643b
Kole Reiser <k.reiser@setonhill.edu>
2023-03-28 15:17:33 -0400

    done

--- light2/ViewController.swift
+++ light2/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    var lightOn = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateUI()
         // Do any additional setup after loading the view.
     }
 
-
-}
+    fileprivate func updateUI() {
+        
+        view.backgroundColor = lightOn ? .white : .black
+            
+        
+    }
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        lightOn.toggle()
+        updateUI()
+        }
+        
+    }
 
