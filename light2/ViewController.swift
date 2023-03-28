abb6727a3f205c6a18ad443f14fa6880e7d810dc
Kole Reiser <k.reiser@setonhill.edu>
2023-03-28 14:57:25 -0400

    Initial Commit

---
+++ light2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  light2
+//
+//  Created by Kole Reiser on 3/28/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
